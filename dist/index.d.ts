0a8762b90dcc0a4f89de7f8e2844569ec69dfd6e
k-w-wang <wk980221@icloud.com>
2021-10-14 17:27:08 +0800

    提交代码

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,2 @@
-declare const _default: {
-    useFetcher: (fetcher: (params: any) => Promise<any>, initialParams?: any) => [import("./useFetcher").IState, (params: any, force?: boolean | undefined) => void, () => void];
-    useModal: (title?: string | undefined, visible?: boolean | undefined, loading?: boolean | undefined, data?: any) => [import("./useModal").IModalConfig, import("./useModal").IModalFun];
-};
-export default _default;
+import useFetcher from "./useFetcher";
+export default useFetcher;
--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var useFetcher_1 = __importDefault(require("./useFetcher"));
-var useModal_1 = __importDefault(require("./useModal"));
-exports.default = {
-    useFetcher: useFetcher_1.default,
-    useModal: useModal_1.default
-};
+exports.default = useFetcher_1.default;
